fa7eaf38b226ba951aa8e24b15c8da2451c9afd9
Harleen Kaur <hk22.work@gmail.com>
2025-07-24 19:26:32 +0530

    Create index.js

---
+++ chat-backend/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+const { Server } = require('socket.io');
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*"
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("chat-message", (data) => {
+    console.log("Message received:", data);
+    socket.broadcast.emit("chat-message", { ...data, sender: "Stranger" });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+server.listen(4000, () => {
+  console.log("Server running at http://localhost:4000");
+});
